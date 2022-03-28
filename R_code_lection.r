f90932d8a7f9620c50ff41321584d518cad10cfe
FrancescoCandini <101177052+FrancescoCandini@users.noreply.github.com>
2022-03-28 17:04:17 +0200

    Update R_code_lection.r

--- R_code_lection.r
+++ R_code_lection.r
@@ -10,6 +10,7 @@ setwd("C:/lab/") # set working directory; codice per caricare i dati nella carte
 landsat2011 <- brick("p224r63_2011.grd") # così carichiamo velocemente il file di interesse
 # setwd ci consente di non specificare tutto il path del file (simil progetto di R)
 landsat2011 <- brick("C:/lab/p224r63_2011.grd") # altrimenti lo specifichiamo
+landsat1988 <- brick("p224r63_1988.grd") # carico l'immagine del 1988 nella stessa cartella precedentemente evidenztiata
 
 landsat2011 # vediamo le informazioni del file, abbiamo circa 3*10^7 dati (pixels)
 # Banda 1 = blu
@@ -27,17 +28,17 @@ cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
 # modifichiamo la legenda base con passaggio graduale tra 100 colori
 # link per tutti i colori di R: https://www.r-graph-gallery.com/42-colors-names.html
 
+cl_b <- colorRampPalette(c("dark blue", "blue", "light blue")) (100) #nuova palette blu
+cl_v <- colorRampPalette(c("dark green", "green", "light green")) (100) #nuova palette verde
+cl_r <- colorRampPalette(c("dark red", "red", "pink")) (100) #nuova palette rossa
+cl_ni <- colorRampPalette(c(" red", "orange", "yellow")) (100) #nuova palette near infrared
+
 plot(landsat2011, col = cl)
 
 # Plottiamo singole bande 
 plot(landsat2011$B1_sre) # indicando il suo nome
 plot(landsat2011[[1]]) # indicando il numero dell'elemento
 plot(landsat2011$B1_sre, col = cl) # comando preferito con palette precedentemente creata
-
-cl_b <- colorRampPalette(c("dark blue", "blue", "light blue")) (100) #nuova palette blu
-cl_v <- colorRampPalette(c("dark green", "green", "light green")) (100) #nuova palette verde
-cl_r <- colorRampPalette(c("dark red", "red", "pink")) (100) #nuova palette rossa
-cl_ni <- colorRampPalette(c(" red", "orange", "yellow")) (100) #nuova palette near infrared
 
 # Esportare grafici in pdf
 pdf("banda_blu.pdf") # nome del file inserito della directory indicata con setwd
@@ -72,3 +73,38 @@ plot(landsat2011$B2_sre, col = cl_v)
 plot(landsat2011$B3_sre, col = cl_r) 
 plot(landsat2011$B4_sre, col = cl_ni) 
 dev.off()
+
+# Colori formati con lo schema RGB (red - green - blue) che limitano la sovrapposizione di 3 bande
+plotRGB(landsat2011, r="B3_sre", g="B2_sre", b="B1_sre", stretch="lin") # plot RGB 
+plotRGB(landsat2011, r=3, g=2, b=1, stretch="lin") # alternativa senza nomi, ma con la posizione degli elementi
+
+plotRGB(landsat2011, r=4, g=3, b=2, stretch="lin") # cancello la banda blu e metto quella dell'infrarosso vicino
+# L'infrarosso vicino (NIR) mostra la presenza di vegetazione
+plotRGB(landsat2011, r=3, g=4, b=2, stretch="lin") # metto la banda NIR con il colore verde
+# Stretch lineare: costruisco una funzione lineare che relativizzano i valori ottenuti dal minimo al massimo
+# Stretch histogram: utilizza una funzione iperbolica (?) per distinguere più facilmente colori simili nei valori medi 
+plotRGB(landsat2011, r=3, g=4, b=2, stretch="hist")
+
+# multiframe di immagini a colori reali con diverso stretch
+par(mfrow=c(2, 1))
+plotRGB(landsat2011, r=3, g=2, b=1, stretch="lin") # alternativa senza nomi, ma con la posizione degli elementi
+plotRGB(landsat2011, r=3, g=2, b=1, stretch="hist") # alternativa senza nomi, ma con la posizione degli elementi
+
+dev.off()
+
+# Multiframe di confronto tra le immagini di età diverse
+par(mfrow=c(2, 1))
+plotRGB(landsat2011, r=3, g=2, b=1, stretch="lin") # alternativa senza nomi, ma con la posizione degli elementi
+plotRGB(landsat1988, r=3, g=2, b=1, stretch="lin") # alternativa senza nomi, ma con la posizione degli elementi
+
+dev.off()
+
+par(mfrow=c(2, 1))
+plotRGB(landsat2011, r=4, g=3, b=2, stretch="lin") # alternativa senza nomi, ma con la posizione degli elementi
+plotRGB(landsat1988, r=4, g=3, b=2, stretch="lin") # alternativa senza nomi, ma con la posizione degli elementi
+
+
+
+# Indici di vegetazione per misurare lo stato di salute delle piante e confronto nel tempo
+
+
