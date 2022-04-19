0a7ae6072e770aede91d9c011f24d8261a17c03e
FrancescoCandini <101177052+FrancescoCandini@users.noreply.github.com>
2022-04-19 11:30:33 +0200

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -1,6 +1,8 @@
+# install.packages("...") azione precedente necessaria per ogni pacchetto
 
 library(raster)
 library(rgdal)
+library(RStoolbox)
 
 setwd("C:/lab/") 
 
@@ -33,7 +35,7 @@ plotRGB(landsat2006 ,r=1, g=2, b=3, stretch="lin")
 dev.off()
 
 # Different Vegetation Index (DVI)
-# DVI = NIR - red 
+# DVI = NIR - red ; quindi il DVI (ad 8 bit) va da -255 a 255
 cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
 
 dvi1992 = landsat1992[[1]] - landsat1992[[2]]
@@ -50,4 +52,40 @@ dev.off()
 dvi_dif = dvi2006 - dvi1992 # warning per dimensione lievemente diversa tra le immagini
 
 plot(dvi_dif, col=colorRampPalette(c("blue", "white", "red")) (100))
+
+dev.off()
+
+# DVI standardizzato = NDVI (8 bit) che va da -1 ad 1 (serve a fare confronti tra risoluzioni diverse)
+# A 16 bit ci sono 65536 valori, quindi il DVI va da -65535 a 65535, ma il NDVI varia sempre tra -1 ed 1
+# NDVI = DVI / (somma delle componenti del DVI)
+# Esempio
+dvi2006 = landsat2006[[1]] - landsat2006[[2]]
+ndvi2006 = (landsat2006[[1]] - landsat2006[[2]]) / (landsat2006[[1]] + landsat2006[[2]])
+ndvi1992 = (landsat1992[[1]] - landsat1992[[2]]) / (landsat1992[[1]] + landsat1992[[2]])
+ndvi2006
+ndvi1992
+
+# Multiframe con plotRGB sopra all'immagine DVI
+par(mfrow=c(2, 1))
+plotRGB(landsat1992, r=1, g=2, b=3,stretch="lin")
+plot(ndvi1992, col=cl)
+# Il fiume (nel periodo della foto) ha molti solidi disciolti, quindi assue un colore simile al terreno deforestato
+dev.off()
+
+# Multiframe con NDVI1992ve NDVI2006
+par(mfrow=c(2, 1))
+plot(ndvi1992, col=cl)
+plot(ndvi2006, col=cl)
+
+dev.off()
+
+# Calcolo automatico degli spectral indices (necessario RLtoolbox)
+si1992 <- spectralIndices(landsat1992, green=3, red=2, nir=1)
+plot(si1992) # plot di tutti gli indici possibili calcolabili, sia per naturalisti che per geologi
+
+# Idem per il 2006
+si2006 <- spectralIndices(landsat2006, green=3, red=2, nir=1)
+plot(si2006)
+
+
 
