0e4742778d0c93e2f1d0fe489f900c4f8a84c4ad
FrancescoCandini <101177052+FrancescoCandini@users.noreply.github.com>
2022-05-19 11:14:52 +0200

    Update R_code_EU_pollution_during_lockdown.r

--- R_code_EU_pollution_during_lockdown.r
+++ R_code_EU_pollution_during_lockdown.r
@@ -16,9 +16,9 @@ rlist <- list.files(pattern="EN") # lista di tutti i files con il pattern comune
 rimp <- lapply(rlist, raster) # applico la funzione raster a tutti i files della lista (importo tutto)
 en <- stack(rimp) # variabile contenente tutti i files, così da usarli contemporaneamente
 
-plot(en, col=cl)
+plot(en, col=cl) # plot di tutti i files
 
-# Confronto tra il 1o file e l'ultimo
+# Confronto tra il primo file e l'ultimo
 par(mfrow=c(1,2))
 plot(en[[1]], col=cl)
 plot(en[[13]], col=cl)
