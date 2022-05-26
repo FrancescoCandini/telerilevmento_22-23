6faf0c73aa0b305ca906fcd48db741d2309a8fa1
FrancescoCandini <101177052+FrancescoCandini@users.noreply.github.com>
2022-05-26 18:23:04 +0200

    Update R_code_land_cover.r

--- R_code_land_cover.r
+++ R_code_land_cover.r
@@ -2,10 +2,10 @@
 
 # install.packages("patchwork")
 
-library(raster)
+library(raster) # per l'importazione di raster
 library(RStoolbox) # per la classificazione
 library(ggplot2) # per visualizzare meglio i dati
-library(patchwork)
+library(patchwork) # per facilitare la creazione di multiframe
 
 setwd("C:/lab/defor") # set working directory; codice per caricare i dati nella cartella desiderata
 
@@ -30,7 +30,7 @@ ggRGB(l06, 1, 2, 3, stretch="lin")
 p1 <- ggRGB(l92, 1, 2, 3, stretch="lin")
 p2 <- ggRGB(l06, 1, 2, 3, stretch="lin")
 
-p1 + p2 # immagini affiancate
+p1 + p2 # immagini affiancate grazie a patchwork
 p1 / p2 # immagini una sopra l'altra
 
 # Classificazione
@@ -46,6 +46,57 @@ plot(l06c$map)
 freq(l92c$map) # classe 1: 305357 - classe 2: 35935
 freq(l06c$map) # classe 1: 178176 - classe 2: 164550
 
+# Calcolo delle proporzioni tra le classi
+l92 # vedo il numero totale di pixels
+tot92 <- 341292 # n. tot di pixels dell'immagine
+prop_forest_92 <- 305357 /  tot92 # proporzione
+perc_forest_92 <- 305357 * 100 / tot92 # percentuale
+perc_forest_92
 
+perc_agr_92 <- 100 - perc_forest_92 # calcolo la percentuale agricola tramite sottrazione (ho solo 2 classi)
+perc_agr_92
 
-# fine lezione 22/4
+l06
+tot06 <- 342726
+perc_forest_06 <- 178176 * 100 / tot06 # percentuale
+perc_forest_06
+
+perc_agr_06 <- 100 - perc_forest_06 
+perc_agr_06
+
+# Dati finali:
+# perc_forest_92 = 89.47089 %
+# perc_forest_06 = 51.98789 %
+# perc_agr_92 = 10.52911 %
+# perc_agr_06 = 48.01211 %
+
+# La virgola si scrive col punto in inglese/amercicano
+
+# Dataframe: tabella righe * colonne
+# Creo le colonne (fields)
+class <- c("Forest", "Agricolture")
+percent_1992 <- c(89.47089, 10.52911)
+percent_2006 <- c(51.98789, 48.01211)
+
+multitemporal <- data.frame(class, percent_1992, percent_2006)
+multitemporal
+View(multitemporal) # comando per visualizzare la tabella in modo più chiaro
+
+# 1992
+ggplot(multitemporal, aes(x=class, y=percent_1992, color=class)) +
+  geom_bar(stat="identity", fill="white")
+# Creo un nuovo ggplot, imposto il dataframe come fonte dei dati, 
+# poi seleziono le colonne che costituiranno il grafico
+# Con il + indico la somma con un'altra funzione 
+
+# 2006
+ggplot(multitemporal, aes(x=class, y=percent_2006, color=class)) +
+  geom_bar(stat="identity", fill="white")
+
+# Ottenere un pdf, lo troveremo nella cartella indicata all'inizio del codice
+pdf("percentages_2006.pdf")
+ggplot(multitemporal, aes(x=class, y=percent_2006, color=class)) +
+  geom_bar(stat="identity", fill="white")
+dev.off()
+
+# fine lezione 28/4
