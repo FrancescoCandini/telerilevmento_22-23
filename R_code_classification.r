395608cef0ed2f559b9f513dec57f2d5a9429abb
Francesco Candini <101177052+FrancescoCandini@users.noreply.github.com>
2022-07-11 22:13:54 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -1,4 +1,4 @@
-# Classificazione Sole
+# Classification Sun
 
 library(raster)
 library(RStoolbox)
@@ -10,7 +10,7 @@ so <- brick("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
 plotRGB(so, 1, 2, 3, stretch="lin")
 plotRGB(so, 1, 2, 3, stretch="hist")
 
-# Classificazione dei dati solari
+# Classification of solar data
 cl <- colorRampPalette(c('yellow','black','red'))(100)
 soc <- unsuperClass(so, nClasses=3) #
 plot(soc$map, col=cl)
@@ -18,11 +18,9 @@ plot(soc$map, col=cl)
 # set.seed can be used for repeating the experiment in the same manner for N times
 # http://rfunction.com/archives/62
 
-# fine lez. 7/4
 
 
-
-# CLassificazione Grand Canyon
+# CLassification Grand Canyon
 
 gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
 gc
@@ -30,29 +28,27 @@ gc
 plotRGB(gc, 1, 2, 3, stretch="lin")
 plotRGB(gc, r=1, g=2, b=3, stretch="hist")
 
-# Classificazione
-gcclass2 <- unsuperClass(gc, nClasses=2) # unsuper = unsupervised, cioé classificazione automatica (più veloce)
+# Classification
+gcclass2 <- unsuperClass(gc, nClasses=2) # unsuper = unsupervised, automatic classificatione (faster)
 gcclass2
-plot(gcclass2$map) # create 2 classi (legenda superflua) tra alcune rocce ed acqua+ombre
+plot(gcclass2$map) # 2 classes were created (unuseful legend) between some rocks and others + water + shadows
 
-# set.seed(17) funzione per mantenere gli stessi pixels di campione tra i vari plot 
+# set.seed(17) function to mantein the same sample pixels between the plots
 
-
-# Classificazione a 4
+# Classification with 4 classes
 gcclass4 <- unsuperClass(gc, nClasses=4)
 gcclass4
 
 clc <- colorRampPalette(c('yellow','red','blue','black'))(100)
 
 plot(gcclass4$map, col=clc)
-# Classe con nuvole e sabbia, un'altra con ombre ed acqua ed altre 2 con 2 diversi tipi di roccia (probabilmente)
-# Questa classificazione è utile per poi fare un sopralluogo e verificare le classi
-# Le classi cambiano ad ogni plot
+# Class 1: clouds and sand
+# Class 2: shadows and water
+# Class 3 and 4: 2 different types of rocks (probably)
+# This classification is useful to make an overview, later it's needed to verify the classes in person
+# Classes change at every plot (different sample in every plot)
 
-# comparare la mappa classificata con quella originale
+# Compare the map classified with the original
 par(mfrow=c(2,1))
 plot(gcclass4$map, col=clc)
-plotRGB(gc, r=1, g=2, b=3, stretch="hist") # hist per avere un'immagine più chiara
-
-
-# fine lez. 21/4
+plotRGB(gc, r=1, g=2, b=3, stretch="hist") # hist to have a better image
