bb69a3308ab488646b1947c36acfac219785dac6
Francesco Candini <101177052+FrancescoCandini@users.noreply.github.com>
2022-07-13 22:59:12 +0200

    Update R_code_analysis_for_the_exam.r

--- R_code_analysis_for_the_exam.r
+++ R_code_analysis_for_the_exam.r
@@ -1,4 +1,7 @@
-#install.packages("patchwork") and all the other packages
+# Images of ..., the 1st was captured on ... and the 2nd on ...
+
+
+# install.packages("patchwork") and all the other packages
 
 library(raster) # 
 library(RStoolbox) # 
