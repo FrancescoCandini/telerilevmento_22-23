96cb7fbba1687cb4c27783391e0f9bcd8568c45b
Francesco Candini <101177052+FrancescoCandini@users.noreply.github.com>
2022-07-13 17:07:06 +0200

    Update R_code_sdm.r

--- R_code_sdm.r
+++ R_code_sdm.r
@@ -3,8 +3,7 @@
 # "in funzione delle" enviromental variables, called predictors ("predittori")
 # We start from the sampling and a map of distribution of the species to arrive at a map of probability
 
-# install.packages("sdm")
-# install.packages("rgdal")
+# install.packages("sdm") and all the other packages
 
 library(sdm)
 library(raster) 
@@ -26,3 +25,61 @@ occ <- species$Occurrence
 plot(species[occ == 1,], col = "blue", pch = 16)
 points(species[occ == 0,], col = "orange", pch = 16) # function to add points to a previous graph
 
+path <- system.file("external", package = "sdm") 
+# I create a variable to simplify the finding of the files
+
+lst <- list.files(path = path, pattern = 'asc$', full.names = T) # list of predictors
+# full.names is needed in case you want to maintain the whole path in the name of the file
+
+# list brick stack
+# brick isn't needed beacuse the predictors are inside R
+
+preds <- stack(lst)
+preds # like predictors
+
+cl <- colorRampPalette(c("blue", "orange", "red", "yellow"))(100)
+
+# Plots of predictors
+plot(preds, col = cl)
+
+elev <- preds$elevation
+prec <- preds$precipitation
+temp <- preds$temperature
+vege <- preds$vegetation
+
+plot(elev, col=cl)
+points(species[occ == 1,], pch = 19)
+
+plot(prec, col=cl) + points(species[occ == 1,], col = "black", pch = 19)
+
+plot(temp, col=cl) + points(species[occ == 1,], col = "black", pch = 16)
+
+plot(vege, col=cl) + points(species[occ == 1,], col = "black", pch = 16)
+
+# Creating sdm data
+datasdm <- sdmData(train = species, predictors = preds)
+datasdm
+
+m1 <- sdm(Occurrence ~ elevation + precipitation + temperature + vegetation, 
+    data = datasdm, methods = "glm") 
+m1
+
+# Prediction
+p1 <- predict(m1, newdata = preds)
+
+plot(p1, col = cl) + points(species[occ == 1,], col = "black", pch = 16)
+# Comparison between prediction and last sampling
+
+# Other methods: leave one out, bootsteps (?)
+
+par(mfrow=c(2, 3))
+plot(p1, col= cl)
+plot(temp, col = cl)
+plot(prec, col = cl)
+plot(elev, col = cl)
+plot(vege, col = cl)
+plot(p1, col= cl) + points(species[occ == 1,], col = "black", pch = 16) # extra
+
+# Alternative
+final <- stack(preds, p1)
+plot(final, col = cl)
