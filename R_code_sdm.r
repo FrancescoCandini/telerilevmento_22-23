3b723c1451f27925959d42fe95fa4c9d31db73fd
Francesco Candini <101177052+FrancescoCandini@users.noreply.github.com>
2022-07-13 16:00:31 +0200

    Create R_code_sdm.r

---
+++ R_code_sdm.r
@@ -0,0 +1,7 @@
+# Species Distribution Modelling (SDM)
+# It's based on sampling where we extrapolate the (probability of) presence of the species 
+# "in funzione delle" enviromental variables (called "predittori")
+# We start from the sampling and a map of distribution of the species to arrive at a map of probability
+
+install.packages("sdm")
+library(sdm)
