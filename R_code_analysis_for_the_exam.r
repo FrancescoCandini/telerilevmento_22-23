6bf1f5993075ba7dfc055037c4bb2a8a4bc5718b
Francesco Candini <101177052+FrancescoCandini@users.noreply.github.com>
2022-07-14 10:36:58 +0200

    Update R_code_analysis_for_the_exam.r

--- R_code_analysis_for_the_exam.r
+++ R_code_analysis_for_the_exam.r
@@ -1,5 +1,7 @@
 # Images of ..., the 1st was captured on ... and the 2nd on ...
 
+# Landsat band 5 = NIR
+# 
 
 # install.packages("patchwork") and all the other packages
 
@@ -7,9 +9,25 @@ library(raster) #
 library(RStoolbox) # 
 library(ggplot2) # 
 library(patchwork) # 
+library(viridis)
 
 setwd
 
 # Sentinel 2
 s2 <- raster
+s2
 
+# Landsat
+l8 <- raster
+l8
+
+# Landsat 8 images
+# LC08_L2SP_008047_20220702_20220707_02_T1
+# LC08_L2SP_008047_20220616_20220629_02_T1
+# LC09_L2SP_008047_20220608_20220610_02_T1
+# LC08_L2SP_008047_20220531_20220609_02_T1
+# LC09_L2SP_008047_20220523_20220525_02_T1
+# LC09_L2SP_008047_20220507_20220509_02_T1
+# ...
+# LC08_L2SP_008047_20181011_20200830_02_T1
+# LC08_L2SP_008047_20180909_20200830_02_T1
