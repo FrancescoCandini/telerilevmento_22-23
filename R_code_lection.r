5b89b0ff7560b58e6a9f6f0bcacfe7448b5d2fe9
Francesco Candini <101177052+FrancescoCandini@users.noreply.github.com>
2022-07-17 19:05:17 +0200

    Update R_code_lection.r

--- R_code_lection.r
+++ R_code_lection.r
@@ -1,63 +1,56 @@
-# Questo è il primo script che useremo a lezione
-
 # install.packages("raster")
 
 library(raster)
 
-setwd("C:/lab/") # set working directory; codice per caricare i dati nella cartella "lab"
-#gli / in Windows vengono copiati al contrario
+setwd("C:/lab/") # set working directory; to link the folder "lab" where i can take files and save others
 
 landsat2011 <- brick("p224r63_2011.grd") # così carichiamo velocemente il file di interesse
-# setwd ci consente di non specificare tutto il path del file (simil progetto di R)
-landsat2011 <- brick("C:/lab/p224r63_2011.grd") # altrimenti lo specifichiamo
-landsat1988 <- brick("p224r63_1988.grd") # carico l'immagine del 1988 nella stessa cartella precedentemente evidenztiata
+# thx to setwd we can no specify the full path  
+landsat2011 <- brick("C:/lab/p224r63_2011.grd") # or we can specify all
+landsat1988 <- brick("p224r63_1988.grd") 
 
-landsat2011 # vediamo le informazioni del file, abbiamo circa 3*10^7 dati (pixels)
-# Banda 1 = blu
-# Banda 2 = verde
-# Banda 3 = rosso
-# Banda 4 = infrarosso vicino (near infrared)
-# Banda 5 = 
-# Banda 6 = 
-# Banda 7 = 
+landsat2011 # see the file indo, we have 3*10^7 pixels (or data)
+# Band 1 = blue
+# Band 2 = green
+# Band 3 = red
+# Band 4 = near infrared
 
-
-plot(landsat2011) # plot generale X-Y, specificando gli assi (lo stesso file per noi)
+plot(landsat2011) # general plot X-Y
 
 cl <- colorRampPalette(c("black", "grey", "light grey")) (100) 
-# modifichiamo la legenda base con passaggio graduale tra 100 colori
-# link per tutti i colori di R: https://www.r-graph-gallery.com/42-colors-names.html
+# I change the color scale with a gradation of 100 colors
+# link to all the colors in R: https://www.r-graph-gallery.com/42-colors-names.html
 
-cl_b <- colorRampPalette(c("dark blue", "blue", "light blue")) (100) #nuova palette blu
-cl_v <- colorRampPalette(c("dark green", "green", "light green")) (100) #nuova palette verde
-cl_r <- colorRampPalette(c("dark red", "red", "pink")) (100) #nuova palette rossa
-cl_ni <- colorRampPalette(c(" red", "orange", "yellow")) (100) #nuova palette near infrared
+cl_b <- colorRampPalette(c("dark blue", "blue", "light blue")) (100) # new palette blue
+cl_v <- colorRampPalette(c("dark green", "green", "light green")) (100) # new palette green
+cl_r <- colorRampPalette(c("dark red", "red", "pink")) (100) # new palette red
+cl_ni <- colorRampPalette(c(" red", "orange", "yellow")) (100) # new palette near infrared
 
 plot(landsat2011, col = cl)
 
-# Plottiamo singole bande 
-plot(landsat2011$B1_sre) # indicando il suo nome
-plot(landsat2011[[1]]) # indicando il numero dell'elemento
-plot(landsat2011$B1_sre, col = cl) # comando preferito con palette precedentemente creata
+# Plotof the single bands
+plot(landsat2011$B1_sre) # I write the name of the raster and link (with $) the sigle element
+plot(landsat2011[[1]]) # or specifying the number
+plot(landsat2011$B1_sre, col = cl)
 
-# Esportare grafici in pdf
-pdf("banda_blu.pdf") # nome del file inserito della directory indicata con setwd
-plot(landsat2011$B1_sre, col = cl_b) # indico il file da salvare
-dev.off() # chiudo il grafico che creo
+# Export graphs .pdf
+pdf("banda_blu.pdf") # file name inserted in the folder setted with sewd
+plot(landsat2011$B1_sre, col = cl_b) # I specify the file to save
+dev.off() # I close the graph
 
-# Struttura identica cambiando il formato in png
-png("banda_blu.png") # nome del file inserito della directory indicata con setwd
-plot(landsat2011$B1_sre, col = cl_b) # indico il file da salvare
-dev.off()
+# Export graphs .png
+png("banda_blu.png") # file name inserted in the folder setted with sewd
+plot(landsat2011$B1_sre, col = cl_b) # I specify the file to save
+dev.off() # I close the graph
 
-# Scansione multiframe
-par(mfrow=c(2,1)) # mfrow multiframe row, indichiamo le righe (prima) e le colonne del multiframe
-# Alternativa con grafici a colonne: mfrow=c(1,2)
+# Multiframe scanning
+par(mfrow=c(2,1)) # mfrow multiframe row, to set the multiframe's rows (first) and the columns 
+# Or the opposite: mfcol=c(1,2)
 plot(landsat2011$B1_sre, col = cl) 
 plot(landsat2011$B1_sre, col = cl_b) 
 dev.off()
 
-# Multiframe a 4 grafici
+# Multiframe with 4 graphs
 par(mfrow=c(2,2))
 plot(landsat2011$B1_sre, col = cl_b) 
 plot(landsat2011$B2_sre, col = cl_v) 
@@ -65,7 +58,7 @@ plot(landsat2011$B3_sre, col = cl_r)
 plot(landsat2011$B4_sre, col = cl_ni) 
 dev.off()
 
-# Esportare multiframe
+# Export the multiframe
 pdf("multiframe_4_bande.pdf")
 par(mfrow=c(2,2))
 plot(landsat2011$B1_sre, col = cl_b) 
@@ -74,42 +67,31 @@ plot(landsat2011$B3_sre, col = cl_r)
 plot(landsat2011$B4_sre, col = cl_ni) 
 dev.off()
 
-# Colori formati con lo schema RGB (red - green - blue) che limitano la sovrapposizione di 3 bande
+# Colors RGB (red - green - blue) that limits the overlap at 3 bands
 plotRGB(landsat2011, r="B3_sre", g="B2_sre", b="B1_sre", stretch="lin") # plot RGB 
-plotRGB(landsat2011, r=3, g=2, b=1, stretch="lin") # alternativa senza nomi, ma con la posizione degli elementi
+plotRGB(landsat2011, r=3, g=2, b=1, stretch="lin") # alternative with no-name, but with the posistion of the elements
 
-plotRGB(landsat2011, r=4, g=3, b=2, stretch="lin") # cancello la banda blu e metto quella dell'infrarosso vicino
-# L'infrarosso vicino (NIR) mostra la presenza di vegetazione
-plotRGB(landsat2011, r=3, g=4, b=2, stretch="lin") # metto la banda NIR con il colore verde
-# Stretch lineare: costruisco una funzione lineare che relativizzano i valori ottenuti dal minimo al massimo
-# Stretch histogram: utilizza una funzione iperbolica (?) per distinguere più facilmente colori simili nei valori medi 
+plotRGB(landsat2011, r=4, g=3, b=2, stretch="lin") # remove the blue to put the NIR
+# NIR allows us to see the vegetation
+plotRGB(landsat2011, r=3, g=4, b=2, stretch="lin") # NIR in green position
+# Linear setretch: linear function from min to max obtained value
+# histogram setretch: hyperbolic function to easily distinguish the values near the mean
 plotRGB(landsat2011, r=3, g=4, b=2, stretch="hist")
 
-# multiframe di immagini a colori reali con diverso stretch
+# Multiframe with images with different stretch
 par(mfrow=c(2, 1))
-plotRGB(landsat2011, r=3, g=2, b=1, stretch="lin") # alternativa senza nomi, ma con la posizione degli elementi
-plotRGB(landsat2011, r=3, g=2, b=1, stretch="hist") # alternativa senza nomi, ma con la posizione degli elementi
+plotRGB(landsat2011, r=3, g=2, b=1, stretch="lin") # no-name, but posistion of the elements
+plotRGB(landsat2011, r=3, g=2, b=1, stretch="hist") # no-name, but posistion of the elements
 
 dev.off()
 
-# Multiframe di confronto tra le immagini di età diverse
+# Multiframe to compare images at different time
 par(mfrow=c(2, 1))
-plotRGB(landsat2011, r=3, g=2, b=1, stretch="lin") # alternativa senza nomi, ma con la posizione degli elementi
-plotRGB(landsat1988, r=3, g=2, b=1, stretch="lin") # alternativa senza nomi, ma con la posizione degli elementi
+plotRGB(landsat2011, r=3, g=2, b=1, stretch="lin")
+plotRGB(landsat1988, r=3, g=2, b=1, stretch="lin") 
 
 dev.off()
 
 par(mfrow=c(2, 1))
-plotRGB(landsat2011, r=4, g=3, b=2, stretch="lin") # alternativa senza nomi, ma con la posizione degli elementi
-plotRGB(landsat1988, r=4, g=3, b=2, stretch="lin") # alternativa senza nomi, ma con la posizione degli elementi
-
-
-
-# Indici di vegetazione per misurare lo stato di salute delle piante e confronto nel tempo
-# Le piante in condizioni di stress le foglie cambiano colore, quindi anche la riflettanza
-# Different Vegetation Index (DVI) è un indice per la riflettanza (a date lunghezze d'onda) per ogni pixel vegetato
-# Il mesofillo fogliare cambia conformazione assorbendo meno rosso ed assorbirà più NIR
-
-
-
-
+plotRGB(landsat2011, r=4, g=3, b=2, stretch="lin")
+plotRGB(landsat1988, r=4, g=3, b=2, stretch="lin")
