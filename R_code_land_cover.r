06800395c51abe6c0464bf2410d878e1e4b3d7e2
Francesco Candini <101177052+FrancescoCandini@users.noreply.github.com>
2022-07-18 23:57:40 +0200

    Update R_code_land_cover.r

--- R_code_land_cover.r
+++ R_code_land_cover.r
@@ -1,254 +1,79 @@
-# R code to analyze 2 satellite images
+# R_code_land_cover.r
 
-library(raster) # to import raster
+# install.packages("ggplot2")
+
+library(raster)
 library(RStoolbox) # to make classifications
-library(ggplot2) # to better visualize the data (with plots)
-library(patchwork) # to build more easily multiframes
-library(viridis) # to use color scales to improve graph readability for who has some forms of color vision deficiency
+library(ggplot2)
+library(gridExtra) # to plot with grid.arrange
 
-setwd("C:/landsat_data")
+# setwd("C:/lab/") # Windows
 
-# List of all the file with the same pattern 
-rlist_14 <- list.files(pattern="LC08_L2SP_008047_20140101")
+# NIR 1, red 2, green 3
 
-# Application of the function "raster" to all the file of the list
-rimp_14 <- lapply(rlist_14, raster)
-
-# Variable with all the images of 2014
-l_14 <- stack(rimp_14) 
-
-# Same process for the images of 2018
-rlist_18 <- list.files(pattern="LC08_L2SP_008047_20180909")
-rimp_18 <- lapply(rlist_18, raster)
-l_18 <- stack(rimp_18)
+defor1 <- brick("defor1.jpg")
+plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
+ggRGB(defor1, r=1, g=2, b=3, stretch="lin")
 
-# Band 2 - Blue
-# Band 3 - Green
-# Band 4 - Red 
-# Band 5 - Near Infrared (NIR)
+defor2 <- brick("defor2.jpg")
+plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
+ggRGB(defor2, r=1, g=2, b=3, stretch="lin")
 
-# Overview of the images
-l_14
-l_18
-# The min and max values of the bands are 0 and 65535, 
-# so these are 16 bit images (65536 possible values)
+par(mfrow=c(1,2))
+plotRGB(defor1, r=1, g=2, b=3, stretch="lin")
+plotRGB(defor2, r=1, g=2, b=3, stretch="lin")
 
-# Plot of all the layers
-plot(l_14, col = magma(65536), axes = F)
-plot(l_18, col = magma(65536), axes = F)
+# Multiframe with ggplot2 and gridExtra
+p1 <- ggRGB(defor1, r=1, g=2, b=3, stretch="lin")
+p2 <- ggRGB(defor2, r=1, g=2, b=3, stretch="lin")
+grid.arrange(p1, p2, nrow=2)
 
-# Plot RGB with the correct bands in RGB
-# I used the linear stretch to visualize the images as we would see from space
-# And I used the histogram stretch to increase the contrast
-par(mfrow=c(2, 2))
-plotRGB(l_14 ,r=4, g=3, b=2, stretch="lin") 
-plotRGB(l_18 ,r=4, g=3, b=2, stretch="lin")
-plotRGB(l_14 ,r=4, g=3, b=2, stretch="hist") 
-plotRGB(l_18 ,r=4, g=3, b=2, stretch="hist")
-mtext("Linear stretch", side = 3, line = -1, outer = T)
-mtext("Histogram stretch", side = 3, line = -19, outer = T)
-
-# Plot RGB with NIR in red
-par(mfrow=c(1, 2))
-plotRGB(l_14 , r=5, g=4, b=3, stretch="lin") 
-plotRGB(l_18 , r=5, g=4, b=3, stretch="lin")
-mtext("RGB plot with NIR on red", side = 3, line = -1, outer = T)
+# Unsupervised classification
+d1c <- unsuperClass(defor1, nClasses=2)
+plot(d1c$map)
+# class 1: forest
+# class 2: agriculture
 
+# Use set.seed() to set a permanent group of pixels to sample in any classification
 
-####################################################################################
-########################## CLASSIFICATION ##########################################
-####################################################################################
+d2c <- unsuperClass(defor2, nClasses=2)
+plot(d2c$map)
+# class 1: agriculture
+# class 2: forest
 
+d2c3 <- unsuperClass(defor2, nClasses=3)
+plot(d2c3$map)
 
+# Frequencies
+freq(d1c$map)
+#   value  count
+# Class 1 = 306583
+# Class 2 = 34709
 
-# Classification in classes
-l_14_c <- unsuperClass(l_14, nClasses = 5)
-l_18_c <- unsuperClass(l_18, nClasses = 5)
+s1 <- 306583 + 34709
 
-l_14_c
-l_18_c
+prop1 <- freq(d1c$map) / s1
+# Proportion forest: 0.8983012
+# Proportion agriculture: 0.1016988
 
-par(mfrow=c(1, 2))
-plot(l_14_c$map , col = viridis(65536), axes = F) 
-plot(l_18_c$map , col = viridis(65536), axes = F)
-mtext("Plot with classification ", side = 3, line = -1, outer = T)
+s2 <- 342726
+prop2 <- freq(d2c$map) / s2
+# Proportion forest: 0.5206958
+# Proportion agriculture: 0.4793042
 
-# Comparing the RGB images with the classified images
-par(mfrow=c(2, 2))
-plotRGB(l_14 ,r=4, g=3, b=2, stretch="lin") 
-plotRGB(l_18 ,r=4, g=3, b=2, stretch="lin")
-plot(l_14_c$map , col = magma(65536), axes = F) 
-plot(l_18_c$map , col = rev(magma(65536)), axes = F) 
-# I used the reversed color scale to have the see with the same color
+# Dataframe
+cover <- c("Forest","Agriculture")
+percent_1992 <- c(89.83, 10.16)
+percent_2006 <- c(52.06, 47.93)
 
-par(mfrow=c(2, 2))
-plotRGB(l_14 ,r=4, g=3, b=2, stretch="lin") 
-plotRGB(l_18 ,r=4, g=3, b=2, stretch="lin")
-plot(l_14_c$map , col = viridis(5), axes = F) 
-plot(l_18_c$map , col = rev(viridis(5)), axes = F) 
+percentages <- data.frame(cover, percent_1992, percent_2006)
+percentages
 
+# Plot the percentages 
+ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white")
+ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity", fill="white")
 
+p1 <- ggplot(percentages, aes(x=cover, y=percent_1992, color=cover)) + geom_bar(stat="identity", fill="white")
+p2 <- ggplot(percentages, aes(x=cover, y=percent_2006, color=cover)) + geom_bar(stat="identity", fill="white")
 
-################################ TEST .......................................
-
-l_14_c2 <- unsuperClass(l_14, nClasses = 2)
-l_18_c2 <- unsuperClass(l_18, nClasses = 2)
-
-l_14_c3 <- unsuperClass(l_14, nClasses = 3)
-l_18_c3 <- unsuperClass(l_18, nClasses = 3)
-
-l_14_c4 <- unsuperClass(l_14, nClasses = 4)
-l_18_c4 <- unsuperClass(l_18, nClasses = 4)
-
-l_14_c5 <- unsuperClass(l_14, nClasses = 5)
-l_18_c5 <- unsuperClass(l_18, nClasses = 5)
-
-par(mfrow=c(2, 2))
-plotRGB(l_14 ,r=4, g=3, b=2, stretch="lin") 
-plotRGB(l_18 ,r=4, g=3, b=2, stretch="lin")
-plot(l_14_c2$map , col = viridis(2), axes = F) # possible use to measure the cloud cover
-plot(l_18_c2$map , col = viridis(2), axes = F)
-plot(l_14_c3$map , col = viridis(3), axes = F) 
-plot(l_18_c3$map , col = viridis(3), axes = F)
-plot(l_14_c4$map , col = viridis(4), axes = F) 
-plot(l_18_c4$map , col = viridis(4), axes = F) 
-plot(l_14_c5$map , col = rev(viridis(5)), axes = F) 
-plot(l_18_c5$map , col = rev(viridis(5)), axes = F) 
-
-############################## END TEST ............................................
-
-
-
-# I use the classification in 2 classes to measure the cloud cover
-l_14_c2 <- unsuperClass(l_14, nClasses = 2)
-l_18_c2 <- unsuperClass(l_18, nClasses = 2)
-
-par(mfrow=c(2, 2))
-plotRGB(l_14 ,r=4, g=3, b=2, stretch="lin") 
-plotRGB(l_18 ,r=4, g=3, b=2, stretch="lin")
-plot(l_14_c2$map , col = viridis(2), axes = F) 
-plot(l_18_c2$map , col = viridis(2), axes = F)
-
-# I calculate the frequencies of the 2 classes and the NA pixels
-freq(l_14_c2$map)
-freq(l_18_c2$map)
-
-# In the end I calculate the percentage of the cloud cover (class 2)
-clouds_14 <- 926022 / 60149911 * 100
-clouds_18 <- 4367844 / 60071601 * 100
-clouds_14
-clouds_18
-
-# Now do the same thing to measure the water cover
-l_14_c3 <- unsuperClass(l_14, nClasses = 3)
-l_18_c3 <- unsuperClass(l_18, nClasses = 3)
-
-par(mfrow=c(2, 2))
-plotRGB(l_14 ,r=4, g=3, b=2, stretch="lin") 
-plotRGB(l_18 ,r=4, g=3, b=2, stretch="lin")
-plot(l_14_c3$map , col = viridis(3), axes = F) 
-plot(l_18_c3$map , col = viridis(3), axes = F)
-
-freq(l_14_c3$map)
-freq(l_18_c3$map)
-
-water_14 <- 12674098 / 60149911 * 100
-water_18 <- 8741281 / 60071601 * 100
-water_14
-water_18
-
-# Continua se le immagini lo permettono.............................................
-
-
-
-
-
-
-dev.off()
-
-
-
-
-
-
-
-
-
-####################################################################################
-######################### SPECTRAL INDICES #########################################
-####################################################################################
-
-
-
-# Calculating the Different Vegetation Index (DVI)
-# It indicates the amount of vegetation in an area (therefore also the biomass)
-# DVI = NIR - red
-# DVI (at 16 bit) goes from -65535 to 65535 
-
-dvi_14 = l_14[[5]] - l_14[[4]]
-dvi_18 = l_18[[5]] - l_18[[4]]
-
-par(mfrow=c(2, 2))
-plotRGB(l_14 ,r=4, g=3, b=2, stretch="lin")
-plotRGB(l_18 ,r=4, g=3, b=2, stretch="lin")  
-plot(dvi_14, col = magma(65536), axes = F)
-plot(dvi_18, col = magma(65536), axes = F)
-mtext("2014                                                                                                            2018", side = 3, line = -1, outer = T)
-
-# Plot to compare the situations of DVI
-par(mfrow=c(1, 2))
-plot(dvi_14, col = magma(65536), main = "DVI 2014 Dominican Republic", axes = F)
-plot(dvi_18, col = magma(65536), main = "DVI 2018 Dominican Republic", axes = F)
-
-# Calculating of the difference in DVI
-
-# dvi_dif = dvi_18 - dvi_14 # warning due to different image extents
-
-# To not have this warning message we can do 
-# a resampling to have the same size and the same extents for both images
-dvi_18r <- resample(dvi_18, dvi_14)
-dvi_perfect_dif = dvi_18r - dvi_14
-
-# It's not necessary to use the DVI standardized (NDVI) 
-# because both the images are al 16 bit 
-
-# Plot of the difference in DIV 
-plot(dvi_perfect_dif, col = magma(65536), 
-     main = "DVI difference (2014-2018), Dominican Republic", axes = F)
-# Higher values mean more vegetation in 2018 then in 2014
-
-
-
-# Automatic calculation of the spectral indices (RStoolbox needed)
-si_18 <- spectralIndices(l_18, blue=2, green=3, red=4, nir=5)
-# Plot of all possible computable indices, both for naturalists and geologists
-plot(si_18, col = magma(65536))
-
-# Same for 2014
-si_14 <- spectralIndices(l_14, blue=2, green=3, red=4, nir=5)
-plot(si_14) # plot of all possible computable indices, both for naturalists and geologists
-
-
-
-####################################################################################
-########################### VARIABILITY ############################################
-####################################################################################
-
-
-
-nir_14 <- l_14[[5]] # dimensions : 7831, 7681, 60149911  (nrow, ncol, ncell)
-nir_18 <- l_18[[5]]
-
-# Aggregate from 30x30 resolution to 150x150 (factor = 4) to make less heavy the next step
-nir_14_agg <- aggregate(nir_14, fact = 5)
-nir_18_agg <- aggregate(nir_18, fact = 5)
-nir_14_agg
-nir_18_agg
-
-# Standard Deviation
-stdev_14 <- focal(nir_14_agg, matrix(1/25, 5, 5), fun=sd)
-stdev_18 <- focal(nir_18_agg, matrix(1/25, 5, 5), fun=sd)
-
-# Plot to see the areas with more or less variability
-plot(stdev_14, col=mako(65536), axes = F)
-plot(stdev_18, col=mako(65536), axes = F)
+grid.arrange(p1, p2, nrow=1)
